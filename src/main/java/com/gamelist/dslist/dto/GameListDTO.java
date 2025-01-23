9a24874d9e322e5139bc09ba1ff38e02eba650ee
Caio Carvalho <caiovcarvalho337@gmail.com>
2025-01-23 17:02:25 -0300

    Deploy ready

--- src/main/java/com/gamelist/dslist/dto/GameListDTO.java
+++ src/main/java/com/gamelist/dslist/dto/GameListDTO.java
@@ -14,7 +14,6 @@ public class GameListDTO {
 		
 	}
 
-
 	public GameListDTO(GameList entity) {
 		BeanUtils.copyProperties(entity, this);
 	}
@@ -23,9 +22,16 @@ public class GameListDTO {
 		return id;
 	}
 
-
 	public String getName() {
 		return name;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 	
 	
