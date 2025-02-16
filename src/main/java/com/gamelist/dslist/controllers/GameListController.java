f649794e244c3113da7eb6aecceaa1aeed5a2692
caiovilquer <caiovcarvalho337@gmail.com>
2025-02-16 18:10:26 -0300

    Missing files fix

--- src/main/java/com/gamelist/dslist/controllers/GameListController.java
+++ src/main/java/com/gamelist/dslist/controllers/GameListController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gamelist.dslist.dto.GameListDTO;
 import com.gamelist.dslist.dto.GameMinDTO;
+import com.gamelist.dslist.dto.ReplacementDTO;
 import com.gamelist.dslist.services.GameListService;
 import com.gamelist.dslist.services.GameService;
 
@@ -17,20 +20,25 @@ import com.gamelist.dslist.services.GameService;
 @RequestMapping(value = "/lists")
 public class GameListController {
 	@Autowired
-    private GameListService gameListService;
-    
-    @Autowired
-    private GameService gameService;
+	private GameListService gameListService;
 
-    @GetMapping
-    public List<GameListDTO> findAll() {
-        List<GameListDTO> result = gameListService.findAll();
-        return result;
-    }
+	@Autowired
+	private GameService gameService;
+	@GetMapping
+	public List<GameListDTO> findAll() {
+		List<GameListDTO> result = gameListService.findAll();
+		return result;
+	}
 
-    @GetMapping(value = "/{listId}/games")
-    public List<GameMinDTO> findByList(@PathVariable Long listId) {
-        List<GameMinDTO> result = gameService.findByList(listId);
-        return result;
-    }
+	@GetMapping(value = "/{listId}/games")
+	public List<GameMinDTO> findByList(@PathVariable Long listId) {
+		List<GameMinDTO> result = gameService.findByList(listId);
+		return result;
+	}
+
+	@PostMapping(value = "/{listId}/replacement")
+	public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body) {
+		gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
+
+	}
 }
