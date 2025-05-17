c82b38049f823ac98a24e5bb43997c698c9a77e3
caiovilquer <caiovcarvalho337@gmail.com>
2025-05-17 14:05:54 -0300

    fix(cors): Simplify CORS configuration with dynamic origins.

--- src/main/java/com/gamelist/dslist/config/WebConfig.java
+++ src/main/java/com/gamelist/dslist/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.gamelist.dslist.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,26 +11,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig {
 
 	@Value("${cors.origins}")
+	private String corsOrigins;
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-
+			public void addCorsMappings(@NonNull CorsRegistry registry) {
+				String[] origins = corsOrigins.split(",");
 				registry.addMapping("/**")
-						.allowedOrigins("https://game-list-frontend.vercel.app/")
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-						.allowedHeaders("*")
-						.allowCredentials(true);
-
-				registry.addMapping("/**")
-						.allowedOrigins("https://gamelist.vilquer.dev/")
+						.allowedOrigins(origins)
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 						.allowedHeaders("*")
 						.allowCredentials(true);
 			}
 		};
 	}
-	
 }
